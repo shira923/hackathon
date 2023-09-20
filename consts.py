e438ba8948b6c2309538523c4e1d349af303038f
shira923 <shirava246@gmail.com>
2023-09-20 12:21:16 +0300

    main

---
+++ consts.py
@@ -0,0 +1,22 @@
+
+BASIC_SUBJECTS = 'לשון תנ"ך אזרחות ספרות היסטוריה מתמטיקה אנגלית'
+BASIC_SUBJECTS_LIST = BASIC_SUBJECTS.split(' ')
+
+SUBJECTS_HIGH = 'חינוך-גופני תקשורת מדעי-המוח רפואה דיפלומטיה רובוטיקה ביוטכנולוגיה קולנוע ערבית מנהל-עסקים גיאוגרפיה מדעי-המחשב פיזיקה כימיה אומנות  ביולוגיה מוזיקה פסיכולוגיה'
+HIGH_SCHOOL_SUBJECT_LIST = SUBJECTS_HIGH.split(' ') + BASIC_SUBJECTS_LIST
+
+SUBJECTS_MIDDLE = 'פיזיקה כימיה מדעי-המחשב ביולוגיה'
+MIDDLE_SCHOOL_SUBJECT_LIST = SUBJECTS_MIDDLE.split(' ') + BASIC_SUBJECTS_LIST
+
+ELEMENTARY_SCHOOL_LIST = BASIC_SUBJECTS_LIST
+
+
+GRADE = 'א ב ג ד ה ו ז ח ט י י"א י"ב'
+GRADE_LIST = GRADE.split(' ')
+
+SUB_SUBJECT = []
+
+DESCRIPTION_LIST = []
+
+NUM_STARS = 5
+
